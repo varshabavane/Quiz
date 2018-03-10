c8d06045b8f1eb6797bf7203f8b0145d17245f10
varsha bavane <varshab13bavane@gmail.com>
2018-03-10 17:55:38 +0530

    functionaliy is added for ans selection

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  counter = 0;
+  isDisable;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  ansChck(ans) {
+    console.log('checked value ' + ans)
+    
+    if (ans === 1) {
+      this.counter = this.counter + 1;
+      
+    } else {
+      this.counter = this.counter;
+    }
   }
 
+  disableQuest(){
+    this.isDisable = true;
+  }
+
+  submit(){
+    alert("score: " + this.counter +" / 10")
+  }
 }
